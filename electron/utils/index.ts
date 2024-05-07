19f7f8df3321ad371b7b6cefd9cc668c697a299a
Henry Ge <1981761687@qq.com>
2024-05-07 09:15:10 +0800

    style: 更改文件名

--- electron/utils/index.ts
+++ electron/utils/index.ts
@@ -2,7 +2,7 @@ import checkAuthorization from './check-authorization'
 import macAddress from './get-mac-address'
 import write2Excel from './write2excel'
 import { getYesterdayDate, getTodayDate } from './get-date'
-import checkPathExists from './checkPathExists'
+import checkPathExists from './check-path-exists'
 import write2config from './write2config'
 
 export {
