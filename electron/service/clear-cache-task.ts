fe6e7910be7de6d7348102b0322567f243daf1cf
Henry Ge <1981761687@qq.com>
2024-05-24 15:22:11 +0800

    refactor: 去掉冗余代码
    因为clear cashe 任务只会运行一次，所以去掉isFirstRun的判断

--- electron/service/clear-cache-task.ts
+++ electron/service/clear-cache-task.ts
@@ -1,15 +1,11 @@
 import { scheduleJob } from 'node-schedule'
 import { getTodayDate, clearCache } from '../utils'
 
-let isFirstRun = true
-
 function runClearCacheTask() {
-  if (isFirstRun) {
-    let task = scheduleJob('5 0 * * *', () => {
-      const executionDate = getTodayDate('')
-      clearCache(executionDate)
-    }).invoke()
-  }
+  scheduleJob('5 0 * * *', () => {
+    const executionDate = getTodayDate('')
+    clearCache(executionDate)
+  }).invoke()
 }
 
 export default runClearCacheTask
